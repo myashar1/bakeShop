3425a7458d5dc32d295db5f350b92f92049e8f91
Melodie Yashar <melodieyashar@Melodies-MacBook-Pro.local>
2017-10-06 02:02:13 -0400

    bakeShop v1

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-    function addNewList() {
+    function addToCart() {
         alert('This item has been added to your cart!');
+        console.log('hello world console');
+}
+
+
+    function submitDetails() {
+        alert('Thank you for your order!');
         console.log('hello world console');
 }
